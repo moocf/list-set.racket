ddd681c3b2e3914f42793102ccb8e77b9c428598
wolfram77 <wolfram77@gmail.com>
2019-11-22 16:17:00 +0530

    function programming on lists

---
+++ main.rkt
@@ -0,0 +1,14 @@
+#lang racket
+
+; (list-set lst n x): L X N X N -> L
+; returns list with n-th element replaced by x
+; : size(lst)=0 -> ()
+; : n=0         -> (x . lst[1..end])
+; : else        -> (lst[0] . (list-set lst[1..end] n-1 x))
+(define list-set
+  (lambda (lst n x)
+    (if (null? lst)
+        (list)
+        (if (zero? n)
+            (cons x (cdr lst))
+            (cons (car lst) (list-set (cdr lst) (- n 1) x))))))
